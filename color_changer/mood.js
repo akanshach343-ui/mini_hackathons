89fe1481bff7527201623dbe285e3a5bce5273df
akanshach343-ui <akanshach343@gmail.com>
2026-05-13 12:41:00 +0530

    Add files via upload

---
+++ color_changer/mood.js
@@ -0,0 +1,15 @@
+document.getElementById("happy").addEventListener('click', ()=>{
+    document.body.style.backgroundColor='yellow';
+});
+
+document.getElementById("calm").addEventListener('click', ()=>{
+    document.body.style.backgroundColor='skyblue';
+});
+
+document.getElementById("angry").addEventListener('click', ()=>{
+    document.body.style.backgroundColor='red';
+})
+
+document.getElementById("neutral").addEventListener('click', ()=>{
+    document.body.style.backgroundColor='beige';
+})
